65661dab2e1c63e218ee27d502751fab64d61a1d
lauSeoHua <winniethepoohseohua@gmail.com>
2025-10-31 20:57:12 +0800

    added_password_login_and_added_stwrite

--- FS_main.py
+++ FS_main.py
@@ -2,14 +2,27 @@
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-
+from utility import check_password
 
 
 def main():
-    
+    # region <--------- Streamlit App Configuration --------->
+    st.set_page_config(
+        layout="centered",
+        page_title="AI Champions Bootcamp Proof Of Concept",
+        initial_sidebar_state="collapsed" 
+    )
+
+    # Do not continue if check_password is not True.  
+    if not check_password():  
+        st.stop()
+
+    # endregion <--------- Streamlit App Configuration --------->
+
     # Create a connection object.
     conn = st.connection("gsheets", type=GSheetsConnection)
 
+    st.write("Hello")
     df = conn.read()
 
     # Print results.
