cd70dc8daa84eddc25b3de9a30c9f130b863777a
lauSeoHua <winniethepoohseohua@gmail.com>
2025-11-01 08:42:59 +0800

    edited_data_analyitcs

--- pages/Data Analytics.py
+++ pages/Data Analytics.py
@@ -22,6 +22,6 @@ numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
 if numeric_columns:
     #selected_column = st.selectbox("Select a numeric column to calculate total:", numeric_columns)
     total = df["Money earned"].sum()
-    st.success(f"✅ {selected_column}: {total:,}")
+    st.success(f"✅ Money earned: {total:,}")
 else:
     st.warning("No numeric columns found in the uploaded data.")
