a23f3ae50b05c8019986fbe38b8463d7b7ed0c2b
lauSeoHua <winniethepoohseohua@gmail.com>
2025-11-01 08:22:05 +0800

    added_data_analytics

---
+++ pages/Data Analytics.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+st.set_page_config(page_title="Statistics")
+
+
+st.title('Statistics')
+
+# --- Connect to Google Sheets ---
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+# --- Read data (optional) ---
+df = conn.read(worksheet="Orders", usecols=[0, 1, 2,3], ttl=5)
+prices_df = conn.read(worksheet="Prices")
+
+# Let user choose a column to calculate total
+numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
+
+if numeric_columns:
+    selected_column = st.selectbox("Select a numeric column to calculate total:", numeric_columns)
+    total = df[selected_column].sum()
+    st.success(f"✅ Total of column **{selected_column}**: {total:,}")
+else:
+    st.warning("No numeric columns found in the uploaded data.")
